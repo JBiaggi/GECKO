99267315c16e5b2915f3855bfe3b9070d4d0e12e
Juliano Sabedotti De Biaggi <jusabe@taltech.ee>
2023-09-05 17:45:09 +0300

    New code for N limiting condition

---
+++ ecYaliGEM/code/NlimitingSim.m
@@ -0,0 +1,59 @@
+adapterLocation = fullfile(findGECKOroot,'ecYaliGEM','ecYaliGEMAdapter.m');
+ModelAdapter = ModelAdapterManager.setDefault(adapterLocation);
+params = ModelAdapter.getParameters();
+
+CNratios = [10 20 30 40 50 60];
+
+ecModel = loadEcModel('ecYaliGEM_FSEOF_pooled.yml');
+
+CS_index = find(strcmpi(ecModel.rxns,'y001808')); % glycerol
+growthPos = find(strcmpi(ecModel.rxns,'xBIOMASS'));
+NsourcePos = find(strcmpi(ecModel.rxns,'y001654')); % NH4
+
+% Adjust glycerol uptake to N-limiting phase compatible value
+ecModel = setParam(ecModel,'eq','y001808',-0.47);
+
+% Set objective for lipids production with constrained biomass
+sol = solveLP(ecModel,1);
+ecModel = setParam(ecModel,'eq','xBIOMASS',-sol.f);
+ecModel = setParam(ecModel,'obj','EXC_OUT_m1640',1);
+sol = solveLP(ecModel);
+
+CNratios = [sol.x(CS_index)*3/sol.x(NsourcePos) CNratios];
+
+growthRates = zeros(1,length(CNratios));
+growthRates(1) = sol.x(growthPos);
+
+lipidProduction = zeros(1,length(CNratios));
+lipidProduction(1) = sol.f;
+
+for i = 2:length(CNratios)
+    ecModel = setParam(ecModel,'lb',{'xBIOMASS', 'y001654'},[0 (-0.47*3)/CNratios(i)]);
+    ecModel = setParam(ecModel,'obj','xBIOMASS',1);
+
+    sol = solveLP(ecModel);
+    ecModel = setParam(ecModel,'eq','xBIOMASS',-sol.f);
+    ecModel = setParam(ecModel,'obj','EXC_OUT_m1640',1);
+    sol = solveLP(ecModel);
+
+    growthRates(i) = sol.x(growthPos);
+    lipidProduction(i) = -sol.f;
+end
+
+% Create a figure for Growth Rate
+figure;
+plot(CNratios, growthRates, 'o-', 'LineWidth', 2);
+xlabel('consumed CN Ratio');
+ylabel('Growth Rate');
+title('Growth Rate vs CN Ratio');
+grid on;
+xlim([0, max(CNratios)]);
+
+% Create a separate figure for Lipid Production
+figure;
+plot(CNratios, lipidProduction, 'o-', 'LineWidth', 2);
+xlabel('consumed CN Ratio');
+ylabel('Lipid Production');
+title('Lipid Production vs CN Ratio');
+grid on;
+xlim([0, max(CNratios)]);
