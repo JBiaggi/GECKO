3763afa28d1279904107d55cf16f4c8966f55f3b
Juliano Sabedotti De Biaggi <jusabe@taltech.ee>
2023-09-26 17:27:57 +0300

    Updated FCC script

--- ecYaliGEM/code/fluxControlCoefficients.m
+++ ecYaliGEM/code/fluxControlCoefficients.m
@@ -10,6 +10,7 @@ sol = solveLP(ecModel,1);
 
 ecModel = setParam(ecModel,'lb','xBIOMASS',-sol.f*0.99);
 
+% Fix carbon source and protein pool uptakes
 ecModel = setParam(ecModel,'eq','y001808',sol.x(strcmp(ecModel.rxns,'y001808')));
 
 ecModel = setParam(ecModel,'obj','EXC_OUT_m1640',1);
@@ -17,6 +18,7 @@ ecModel = setParam(ecModel,'obj','EXC_OUT_m1640',1);
 ecModel_OG = ecModel;
 
 sol = solveLP(ecModel,1);
+ecModel = setParam(ecModel,'eq','prot_pool_exchange',sol.x(strcmp(ecModel.rxns,'prot_pool_exchange')));
 
 lipOG = -sol.f;
 
@@ -32,8 +34,9 @@ for i = 1:numel(ecModel.ec.kcat)
     ecModel = applyKcatConstraints(ecModel);
     sol = solveLP(ecModel,1);
     Qlip = -sol.f;
-    FCCs(i) = 1000*(Qlip-lipOG)/lipOG;
-    ecModel.ec.kcat = ecModel_OG.ec.kcat;
+    %FCCs(i) = 1000*(Qlip-lipOG)/lipOG;
+    FCCs(i) = 
+    ecModel = ecModel_OG;
     progressbar(i/numel(ecModel.ec.kcat))
 end
 progressbar(1)
